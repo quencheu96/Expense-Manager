c69e8631f4f26343326005cc0f15263e9e2e757e
Quentin Cheung <quencheu96@yahoo.ca>
2016-09-17 20:10:27 -0400

    Undid last commit

--- ExpenseManager/app/src/main/java/com/example/quentin/expensemanager/MainActivity.java
+++ ExpenseManager/app/src/main/java/com/example/quentin/expensemanager/MainActivity.java
@@ -26,7 +26,6 @@ import okhttp3.OkHttpClient;
 public class MainActivity extends AppCompatActivity {
 
     SQLiteDBHelper mDBHelper;
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -36,5 +35,69 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         mDBHelper = new SQLiteDBHelper(getApplicationContext());
+
+        new Handler(  ).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                showPopup(MainActivity.this);
+            }
+        }, 1000 );
+
+        SharedPreferences sharedPrefs = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor sharedPrefsEditor = sharedPrefs.edit();
+        sharedPrefsEditor.apply();
+        if (sharedPrefs.contains(getString(R.string.shared_preferences_cycle_start_date)) && sharedPrefs.contains(getString(R.string.shared_preferences_cycle_end_date))){
+            Date currentDate = new Date();
+            Date startDate = new Date(sharedPrefs.getLong(getString(R.string.shared_preferences_cycle_start_date),0));
+            Date endDate = new Date(sharedPrefs.getLong(getString(R.string.shared_preferences_cycle_end_date),0));
+
+            if (currentDate.before(startDate) || currentDate.after(endDate)){
+                new AlertDialog.Builder(this)
+                        .setTitle(getString(R.string.alert_title))
+                        .setMessage(getString(R.string.alert_message))
+                        .setNeutralButton(getString(R.string.alert_button_message), new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                showPopup(MainActivity.this);
+                            }
+                        })
+
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+
+            }
+        }
     }
+
+
+
+    private void showPopup(Activity context) {
+
+        // Inflate the popup_layout.xml
+        LayoutInflater layoutInflater = (LayoutInflater)getBaseContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.calendar_view_layout, null,false);
+        // Creating the PopupWindow
+        final PopupWindow popupWindow = new PopupWindow(
+                layout,context.getWindow().getDecorView().getWidth(),context.getWindow().getDecorView().getHeight()/2);
+
+        popupWindow.setContentView(layout);
+        popupWindow.setOutsideTouchable(false);
+        // Clear the default translucent background
+        popupWindow.setBackgroundDrawable(new BitmapDrawable());
+
+        CalendarView cv = (CalendarView) layout.findViewById(R.id.calendarView);
+
+        cv.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+                // TODO Implement logic
+
+
+            }
+        });
+        popupWindow.showAtLocation(layout, Gravity.CENTER,0,0);
+    }
+
 }
